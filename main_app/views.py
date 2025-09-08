6fab9d0d039c182d9c26fc80bac88d5bb681d5c5
Tia Piol <tiarox02@yahoo.com>
2025-09-08 12:03:17 -0700

    render data in template

--- main_app/views.py
+++ main_app/views.py
@@ -3,8 +3,26 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+cats = [
+    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+        
 def home(request): 
-    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def cat_index(request):
+    return render(request, 'cats/index.html', {'cats': cats})
