0cebaeea0b82e2c7c466c82b6fc1ad181bcff561
Tia Piol <tiarox02@yahoo.com>
2025-09-09 09:48:45 -0700

    added user, registered Cat model, created cat detail route and html page

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Cat
 
 # Create your views here.
-
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
 
         
 def home(request): 
@@ -25,4 +11,9 @@ def about(request):
     return render(request, 'about.html')
 
 def cat_index(request):
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
